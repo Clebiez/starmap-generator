d132b75d97ae775fdff43e99dbe26bce46b5eabf
Clément Le Biez <clement.lebiez@unicorne.cloud>
2025-03-11 09:38:15 -0400

    Deploy to vercel

--- api/main.py
+++ api/main.py
@@ -3,11 +3,13 @@ from plot import Plot
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def generateStarPlot():
     plotStyle = request.args.get('plotStyle', '')
     lat = request.args.get('lat', type=float)
     lng = request.args.get('lng', type=float)
+    date = request.args.get('date', type=str)
     showConstellations = request.args.get('showConstellations', type=bool)
     showMilkyWay = request.args.get('showMilkyWay', type=bool)
     showEquator = request.args.get('showEquator', type=bool)
@@ -17,9 +19,13 @@ def generateStarPlot():
     showGalaxies = request.args.get('showGalaxies', type=bool)
     showMoon = request.args.get('showMoon', type=bool)
     showGridLines = request.args.get('showGridLines', type=bool)
-    
-    plot = Plot(lat=lat, lng=lng, showConstellations=showConstellations, showMilkyWay=showMilkyWay, showEquator=showEquator, showHorizon=showHorizon,showEcliptic=showEcliptic, showOpenCluster=showOpenCluster, showGalaxies=showGalaxies, showMoon=showMoon, showGridLines=showGridLines)
+
+    plot = Plot(lat=lat, lng=lng, date=date, showConstellations=showConstellations, showMilkyWay=showMilkyWay, showEquator=showEquator, showHorizon=showHorizon,
+                showEcliptic=showEcliptic, showOpenCluster=showOpenCluster, showGalaxies=showGalaxies, showMoon=showMoon, showGridLines=showGridLines)
     plot.generatePlot()
+
     b64 = plot.getBase64()
 
-    return f"<img src='data:image/svg+xml;base64,{b64}'/>"
+    return {
+        "image": f"data:image/svg+xml;base64,{b64}"
+    }
--- api/plot.py
+++ api/plot.py
@@ -1,17 +1,35 @@
 from datetime import datetime
-from pytz import timezone
+import pytz
 from starplot import MapPlot, HorizonPlot, Projection, Star, DSO, _
 from starplot.styles import PlotStyle, extensions
-from timezonefinder import TimezoneFinder 
+from timezonefinder import TimezoneFinder
 import io
 import base64
 
+
 class Plot:
-    def __init__(self, lat, lng, showConstellations, showMilkyWay, showEquator, showHorizon, showEcliptic, showGalaxies, showOpenCluster, showMoon, showGridLines):
+    def __init__(self, lat, lng, date, showConstellations, showMilkyWay, showEquator, showHorizon, showEcliptic, showGalaxies, showOpenCluster, showMoon, showGridLines):
         self.lat = lat
         self.lng = lng
-        self.tz = TimezoneFinder().timezone_at(lat=lat, lng=lng)
-        self.dt = datetime.now(timezone(self.tz))
+        tf = TimezoneFinder()
+        self.tz = tf.timezone_at(lat=lat, lng=lng)
+
+        # Si aucune timezone n'est trouvée, utiliser UTC par défaut
+        if self.tz is None:
+            self.tz = "UTC"
+
+        # Obtenir l'objet timezone
+        timezone = pytz.timezone(self.tz)
+        date_utc = datetime.fromisoformat(
+            date.replace("Z", "+00:00"))
+
+        # Convertir l'heure UTC en heure locale
+        self.dt = date_utc.astimezone(timezone)
+
+        # Afficher les résultats
+        print("Heure locale :", self.dt)
+        print("Timezone :", self.tz)
+
         self.showConstellations = showConstellations
         self.showMilkyWay = showMilkyWay
         self.showEquator = showEquator
@@ -28,19 +46,18 @@ class Plot:
             extensions.NORD,
         )
 
-
         self.p = MapPlot(
             projection=Projection.ZENITH,
             lat=self.lat,
             lon=-self.lng,
             dt=self.dt,
             style=style,
-            resolution=800,
+            resolution=600,
             autoscale=True
         )
 
-
-        self.p.stars(where=[_.magnitude < 4.6], where_labels=[_.magnitude < 2.1])
+        self.p.stars(where=[_.magnitude < 4.6],
+                     where_labels=[_.magnitude < 2.1])
 
         # Show azimuth and elevation lines
         if self.showGridLines:
@@ -49,7 +66,7 @@ class Plot:
         # Show constellations lines
         if self.showConstellations:
             self.p.constellations()
-        
+
         # Show horizon, in a circle plot only
         if self.showHorizon:
             self.p.horizon()
@@ -57,18 +74,20 @@ class Plot:
         # Show Sun trace line
         if self.showEcliptic:
             self.p.ecliptic()
-        
+
         # Show celestial equator
         if self.showEquator:
             self.p.celestial_equator()
 
         # Show Galaxies
         if self.showGalaxies:
-            self.p.galaxies(where=[_.magnitude < 9], true_size=False, labels=None)
+            self.p.galaxies(where=[_.magnitude < 9],
+                            true_size=False, labels=None)
 
         # Show Open cluster
         if self.showOpenCluster:
-            self.p.open_clusters(where=[_.magnitude < 9], true_size=False, labels=None)
+            self.p.open_clusters(
+                where=[_.magnitude < 9], true_size=False, labels=None)
 
         # Show milky way interpolation
         if self.showMilkyWay:
@@ -77,7 +96,6 @@ class Plot:
         # Show ... the moon phase seems to not works well.
         if self.showMoon:
             self.p.moon(show_phase=True)
-        
 
         # Constellations labels can be shown here but must be at the end to improve positioning
         if self.showConstellations:
@@ -87,12 +105,12 @@ class Plot:
 
         # self.p.legend()
 
-
     def getBase64(self):
         my_stringIObytes = io.BytesIO()
-        self.p.fig.savefig(my_stringIObytes, format='svg', bbox_inches="tight", pad_inches=0.1, transparent=True)
+        self.p.fig.savefig(my_stringIObytes, format='svg',
+                           bbox_inches="tight", pad_inches=0.1, transparent=True)
         my_stringIObytes.seek(0)
         return base64.b64encode(my_stringIObytes.read()).decode()
-    
+
     def export(self):
         self.p.export("starmap.svg", format="svg", padding=0.1)
